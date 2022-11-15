98704e2018df7e77103615f021ea8eb57db589d9
Viktor Repin <hiddenstmail@gmail.com>
2022-11-15 10:05:02 +0700

    Fix schema types

--- postgresql/schemas/messenger.sql
+++ postgresql/schemas/messenger.sql
@@ -4,8 +4,8 @@ CREATE SCHEMA IF NOT EXISTS messenger_schema;
 
 CREATE TABLE IF NOT EXISTS messenger_schema.chat (
     id SERIAL PRIMARY KEY,
-    sender_id INTEGER UNSIGNED NOT NULL,
-    receiver_id INTEGER UNSIGNED NOT NULL,
+    sender_id INTEGER NOT NULL,
+    receiver_id INTEGER NOT NULL,
     timepoint TIMESTAMP NOT NULL,
     message TEXT NOT NULL
 );
